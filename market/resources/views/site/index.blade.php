9dccdc7ce51c972e0bab27c3be57e5295a74766b
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2024-03-09 22:18:30 -0300

    #2

---
+++ market/resources/views/site/index.blade.php
@@ -0,0 +1,19 @@
+<html>   
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href=""> 
+    </head>
+
+    <body>
+        <header>Boa Compra</header>
+
+        <nav>
+            <a href="">Home</a>
+            <a href="">Sobre</a>
+            <a href="">Produtos</a> 
+            <a href="">Contato</a>
+        </nav>
+    </body>
+</html>
